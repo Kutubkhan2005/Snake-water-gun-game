afdc146befcc54313bea8e43a3cd9cf128c19364
Kutubkhan2005 <69675791+Kutubkhan2005@users.noreply.github.com>
2020-09-01 12:05:32 +0530

    Update Snake water gun game.py.py

--- Snake water gun game.py.py
+++ Snake water gun game.py.py
@@ -1,3 +1,4 @@
+#Importing random comment
 import random
 A=["s","w","g"]
 total_chance=10
@@ -9,33 +10,40 @@ print("s for snake\nw for water\ng for gun")
 while chance<total_chance:
     b=input("Snake , water , gun chose any one\n")
     c=random.choice(A)
+    #----------It is when user and comp chooses same---------
     if b==c:
         print("It is a tie")
+    #----------It is when user choose s and comp choose w----------
     elif b=="s" and c=="w":
         h_point=h_point+1
         print(f"Your guess is\t{b} and computer guess is\t{c}")
         print("You win one point")
         print(f"Your point is {h_point} and comp. point is {c_point}")
+    #----------It is when user choose s and comp choose g----------
     elif b=="s" and c=="g":
         c_point=c_point+1
         print(f"Your guess is\t{b} and computer guess is\t{c}")
         print("Comp win one point")
-        print(f"Your point is {h_point} and comp. point is {c_point}")    
+        print(f"Your point is {h_point} and comp. point is {c_point}") 
+    #----------It is when user choose w and comp choose s----------
     elif b=="w" and c=="s":
         c_point=c_point+1
         print(f"Your guess is\t{b} and computer guess is\t{c}")
         print("Comp win one point")
         print(f"Your point is {h_point} and comp. point is {c_point}")
+    #----------It is when user choose w and comp choose g----------
     elif b=="w" and c=="g":
         h_point=h_point+1
         print(f"Your guess is\t{b} and computer guess is\t{c}")
         print("You win one point")
         print(f"Your point is {h_point} and comp. point is {c_point}")
+    #----------It is when user choose g and comp choose w----------
     elif b=="g" and c=="w":
         c_point=c_point+1
         print(f"Your guess\t{b} and computer guess is\t{c}")
         print("Comp win one point")
         print(f"Your point is {h_point} and comp. point is {c_point}")
+    #----------It is when user choose g and comp choose s----------
     elif b=="g" and c=="s":
         h_point=h_point+1
         print(f"Your guess is\t{b} and computer guess is\t{c}")
